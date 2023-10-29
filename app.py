fc180f852ce9d1f5df9fdd76970503e541316d99
“vaibhavkumawat-17” <vaibhav.kumawat017@gmail.com>
2023-10-29 23:15:41 +0530

    update app.py
    Header is hidden but now change it to shoe

--- app.py
+++ app.py
@@ -347,7 +347,6 @@ hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
-            header {visibility: hidden;}
             </style>
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
